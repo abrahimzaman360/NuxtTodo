1596ddc6b2656f58fa08cae124984d0de6c79f6b
AbrahimZaman360 <AbrahimZaman3@gmail.com>
2023-03-02 12:09:47 +0500

    Commited

--- README.md
+++
@@ -1,2 +0,0 @@
-# NuxtTodo - Starter Code is Published on Repository!
-* Blah Blah Blah Later Tell You What's up
--- components/Navbar/index.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <div class="flex flex-col sm:flex-row justify-between bg-slate-600 px-4 py-2 sm:items-center align-middle">
-        <NuxtLink class="text-blue-300 dark:text-white bg-inherit font-bold text-lg" to="#">Todos App</NuxtLink>
-        <NuxtLink class="text-yellow-400 font-semibold bg-inherit dark:text-white" to="/">Change Theme</NuxtLink>
-    </div>
-</template>
